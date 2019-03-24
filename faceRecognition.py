c8226729c27465766d1940bf39fce0a35bbc9f35
Daniel Rothfusz <dpr7@rice.edu>
2019-03-24 14:11:18 -0500

    Face Recognition show image now
    Now should show image with faces boxed on computer screen

--- faceRecognition.py
+++ faceRecognition.py
@@ -49,14 +49,20 @@ class webcam_image:
             eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
             for (x, y, w, h) in faces:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                roi_gray = gray[y:y + h, x:x + w]
-                roi_color = frame[y:y + h, x:x + w]
-                eyes = eye_cascade.detectMultiScale(roi_gray)
-                for (ex, ey, ew, eh) in eyes:
-                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+                # roi_gray = gray[y:y + h, x:x + w]
+                # roi_color = frame[y:y + h, x:x + w]
+                # eyes = eye_cascade.detectMultiScale(roi_gray)
+                # for (ex, ey, ew, eh) in eyes:
+                #     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
             # Display the resulting frame
             cv2.imshow('f', frame)
