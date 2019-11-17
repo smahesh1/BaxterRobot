2af18a9acf3c729617a375dffa4f199ab594c2d0
Daniel Rothfusz <dpr7@rice.edu>
2019-11-17 16:20:40 -0600

    Updated Structure plan
    Put plan in place to finish pickup, will need to determine how to pass coords

--- CandyPickupProject/move_arm_node.py
+++ CandyPickupProject/move_arm_node.py
@@ -11,24 +11,32 @@ import baxter_interface
 
 
 def pickup():
-	pass
+    # Move to intermediate position
+
+    # Move to Pickup position
+
+    # Close grippers
+
+    # Move up to intermediate position
+
+    # Move to handout position
 
 
 def arm_setup():
 	### TODO: figure out what values we want for default arm position
     # Get desired joint values from parameter server
-    left_w0 = rospy.get_param('left_w0',default =0)
-    left_w1 = rospy.get_param('left_w1',default =0)
-    left_w2 = rospy.get_param('left_w2',default =0)
-    left_e0 = rospy.get_param('left_e0',default =0)
-    left_e1 = rospy.get_param('left_e1',default =0)
-    left_s0 = rospy.get_param('left_s0',default =0)
-    left_s1 = rospy.get_param('left_s1',default =0)
+    # left_w0 = rospy.get_param('left_w0',default =0)
+    # left_w1 = rospy.get_param('left_w1',default =0)
+    # left_w2 = rospy.get_param('left_w2',default =0)
+    # left_e0 = rospy.get_param('left_e0',default =0)
+    # left_e1 = rospy.get_param('left_e1',default =0)
+    # left_s0 = rospy.get_param('left_s0',default =0)
+    # left_s1 = rospy.get_param('left_s1',default =0)
 
-    # Send the left arm to the desired position
-    home = {'left_w0': left_w0, 'left_w1': left_w1, 'left_w2': left_w2, 'left_e0': left_e0, 'left_e1': left_e1, 'left_s0': left_s0, 'left_s1': left_s1}
+    # # Send the left arm to the desired position
+    # home = {'left_w0': left_w0, 'left_w1': left_w1, 'left_w2': left_w2, 'left_e0': left_e0, 'left_e1': left_e1, 'left_s0': left_s0, 'left_s1': left_s1}
     limb = baxter_interface.Limb('left')
-    limb.move_to_joint_positions(home)
+    limb.move_to_neutral()
 
 
 # Left out some stuff, we can add in if we need it - Daniel
